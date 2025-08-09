f4b91f5ef0245cac985f9ae18d4d704e051e5219
Kursat <75574588+DKursat@users.noreply.github.com>
2025-08-09 23:08:08 +0200

    initial Setup

---
+++ CoreEngine/CoreEngine.cpp
@@ -0,0 +1,12 @@
+﻿// CoreEngine.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "CoreEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
